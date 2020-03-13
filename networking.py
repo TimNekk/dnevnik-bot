8a81ccb3e80f5332fdc21ccb32acdfe2610ff1cf
unknown <herew26@gmail.com>
2020-03-14 02:26:33 +0300

    Marks_page added to get_diary_pages

--- networking.py
+++ networking.py
@@ -1,27 +1,58 @@
-from selenium import webdriver
+from selenium import webdriver, common
 from bs4 import BeautifulSoup as BS
 from time import sleep
-
-login = 'herew26@gmail.com'
-password = 'VYrixCH3F7CXhyi'
+from threading import Thread
 
 
-def get_dairy_soup():
+slogin = 'herew26@gmail.com'
+spassword = 'VYrixCH3F7CXhyi'
+
+timer_work = True
+
+
+def start_timer():
+    i = 0
+    while timer_work:
+        sleep(1)
+        i += 1
+        print(i)
+
+
+def get_diary_page(login, password):
     driver = webdriver.Chrome('chromedriver.exe')
     driver.get("https://login.mos.ru/")
     driver.find_element_by_css_selector('.link_primary__fU-Jd').click()
     driver.find_element_by_id('login').send_keys(login)
     driver.find_element_by_id('password').send_keys(password)
-    driver.find_element_by_id('bind').click()
-    driver.get('https://dnevnik.mos.ru/?token=bcf2931677a1ea467493afb7ab27718b')
-
-    while not driver.find_elements_by_tag_name('diary-student-diary-day'):
-        continue
-
     sleep(1)
-    page = driver.page_source
+    driver.find_element_by_id('bind').click()
+
+    # Получение страницы с расписанием
+    driver.get('https://dnevnik.mos.ru/?token=1673527472d3add00f032929d0a553d4')
+
+    try:
+        driver.find_element_by_id('login').send_keys(login)
+        driver.find_element_by_id('password').send_keys(password)
+        sleep(1)
+        driver.find_element_by_id('bind').click()
+    except common.exceptions.NoSuchElementException:
+        pass
+
+    while not driver.find_elements_by_tag_name('diary-student-diary-day'):  # Ожидание загрузки страницы
+        continue
+    sleep(1)
+    timetable_page = driver.page_source
+
+    # Получение страницы с оценками
+    driver.get('https://dnevnik.mos.ru/progress/all_marks')
+
+    while not driver.find_elements_by_tag_name('.all-marks-data .br-text'):  # Ожидание загрузки страницы
+        continue
+    sleep(1)
+    marks_page = driver.page_source
+
     driver.close()
-    return page
+    return timetable_page, marks_page
 
 
 def scraping(page):
@@ -39,7 +70,8 @@ def scraping(page):
 
 
 def start():
-    page = get_dairy_soup()
-    scraping(page)
+    Thread(target=start_timer).start()
+    timetable_page, marks_page = get_diary_page()
+    # scraping()
 
-start()
+# start()
