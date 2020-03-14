2dcae20162e229ea91faf0ed9716147e55211df7
unknown <herew26@gmail.com>
2020-03-15 00:04:54 +0300

    timetable_page_next & timetable_page_pre & marks_page added

--- networking.py
+++ networking.py
@@ -1,77 +1,89 @@
 from selenium import webdriver, common
-from bs4 import BeautifulSoup as BS
 from time import sleep
-from threading import Thread
+import secrets
+import pickle
+import scraping as s
 
 
 slogin = 'herew26@gmail.com'
 spassword = 'VYrixCH3F7CXhyi'
 
-timer_work = True
 
-
-def start_timer():
-    i = 0
-    while timer_work:
-        sleep(1)
-        i += 1
-        print(i)
-
-
-def get_diary_page(login, password):
+def get_diary_pages(login, password):
     driver = webdriver.Chrome('chromedriver.exe')
-    driver.get("https://login.mos.ru/")
-    driver.find_element_by_css_selector('.link_primary__fU-Jd').click()
-    driver.find_element_by_id('login').send_keys(login)
-    driver.find_element_by_id('password').send_keys(password)
+
+    url = 'https://dnevnik.mos.ru/?token=' + str(secrets.token_hex())  # Генерация url
+
+    driver.get(url)
+    driver.find_element_by_id('login').send_keys(login)  # Ввести логин
+    driver.find_element_by_id('password').send_keys(password)  # Ввести пароль
     sleep(1)
-    driver.find_element_by_id('bind').click()
+    driver.find_element_by_id('bind').click()  # Кнопка войти
 
-    # Получение страницы с расписанием
-    driver.get('https://dnevnik.mos.ru/?token=1673527472d3add00f032929d0a553d4')
+    # Если данные не верные
+    if driver.find_elements_by_css_selector('blockquote ol:last-child, blockquote p:last-child, blockquote ul:last-child'):
+        driver.close()
+        return False
 
+    # Если появилось еще одно окно с авторизацией
     try:
-        driver.find_element_by_id('login').send_keys(login)
-        driver.find_element_by_id('password').send_keys(password)
+        driver.find_element_by_id('login').send_keys(login)  # Ввести логин
+        driver.find_element_by_id('password').send_keys(password)  # Ввести пароль
         sleep(1)
-        driver.find_element_by_id('bind').click()
+        driver.find_element_by_id('bind').click()  # Кнопка войти
     except common.exceptions.NoSuchElementException:
         pass
 
+    # Получение страницы с расписанием
     while not driver.find_elements_by_tag_name('diary-student-diary-day'):  # Ожидание загрузки страницы
         continue
     sleep(1)
-    timetable_page = driver.page_source
+    timetable_page_now = driver.page_source
+
+    # Получение страницы с расписанием на след. неделю
+    old = s.get_week_info(driver.page_source)
+    driver.find_element_by_css_selector('.diary-toolbar[_ngcontent-c19] .next-week-button[_ngcontent-c19]').click()  # Кнопка след. неделя
+
+    # Ожидание загрузки страницы
+    while True:
+        new = s.get_week_info(driver.page_source)
+        if new != old and new:
+            break
+
+    while not driver.find_elements_by_tag_name('diary-student-diary-day'):  # Ожидание загрузки страницы
+        continue
+    timetable_page_next = driver.page_source
+
+    # Получение страницы с расписанием на пред. неделю
+    for _ in range(2):
+        old = s.get_week_info(driver.page_source)
+        driver.find_element_by_css_selector('.mat-stroked-button:not([class*=mat-elevation-z])').click()  # Кнопка пред. неделя
+
+        # Ожидание загрузки страницы
+        while True:
+            new = s.get_week_info(driver.page_source)
+            if new != old and new:
+                break
+
+    while not driver.find_elements_by_tag_name('diary-student-diary-day'):  # Ожидание загрузки страницы
+        continue
+    timetable_page_pre = driver.page_source
 
     # Получение страницы с оценками
     driver.get('https://dnevnik.mos.ru/progress/all_marks')
 
-    while not driver.find_elements_by_tag_name('.all-marks-data .br-text'):  # Ожидание загрузки страницы
+    while not driver.find_elements_by_css_selector('.all-marks-data .br-text'):  # Ожидание загрузки страницы
         continue
     sleep(1)
     marks_page = driver.page_source
 
     driver.close()
-    return timetable_page, marks_page
+    return timetable_page_now, timetable_page_next, timetable_page_pre, marks_page
 
 
-def scraping(page):
-    page = BS(page, 'html.parser')
+if __name__ == '__main__':
+    page = get_diary_pages(slogin, spassword)
 
-    days = page.select('.lessons-wrapper[_ngcontent-c18]')
-    for day in days:
-        lessons = day.select('.lessons-wrapper[_ngcontent-c18] .schedule-item-row[_ngcontent-c18]')
-        for lesson in lessons:
-            name_div = lesson.select('.lessons-wrapper[_ngcontent-c18] .schedule-item-row[_ngcontent-c18] .subject[_ngcontent-c18]')[0]
-            name = name_div.find('span').text
-            # name = name.find_all('div')[0]
-            print(name)
-        print('\n')
+    # with open('pages.txt', 'w') as file:
+    #     file.write(page)
 
-
-def start():
-    Thread(target=start_timer).start()
-    timetable_page, marks_page = get_diary_page()
-    # scraping()
-
-# start()
