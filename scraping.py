78a0f3007354ca54ad56bddb50057bfc19facc22
unknown <herew26@gmail.com>
2020-03-16 01:15:07 +0300

    get_grade_and_school & get_marks & get_periods_count methods was added

--- scraping.py
+++ scraping.py
@@ -9,12 +9,89 @@ def get_page():
     return page
 
 
+def get_page2():
+    with open('pages2.txt', 'rb') as file:
+        page = file.read()
+    return page
+
+
+def get_periods_count(marks_page):
+    page = BS(marks_page, 'html.parser')
+    subject_div = page.select('.subject-periods-wrapper')[0]
+    periods = subject_div.select('.subject-period')
+    periods_count = len(periods)
+    return periods_count
+
+
+# TODO - Сделать
+def get_analysis():
+    pass
+
+
+def get_marks(marks_page, period_count, multiply_marks=False):
+    page = BS(marks_page, 'html.parser')
+
+    subjects_div = page.select('.all-marks-data')  # Предметы
+    subjects = []
+    for subject_div in subjects_div:
+        name = subject_div.select('.all-marks-data .br-text')[0].text  # Название предмета
+
+        try:
+            period_div = subject_div.select('.module-marks')[period_count]  # Пероид
+        except IndexError:
+            return False
+
+        marks = []
+        marks_div = period_div.select('.module-marks-row span')  # Оценки
+        for mark_div in marks_div:
+            mark = mark_div.text
+            mark = re.findall(r'\d', mark)[0]
+
+            # Вес
+            try:
+                weight = mark_div.select('.module-marks-row span sub')[0].text
+
+                if multiply_marks:
+                    mark = str(int(mark) * int(weight))
+                else:
+                    if weight == '2':
+                        mark += '²'
+                    elif weight == '3':
+                        mark += '³'
+                    elif weight == '4':
+                        mark += '⁴'
+                    elif weight == '5':
+                        mark += '⁵'
+            except IndexError:
+                pass
+
+            marks.append(mark)
+        avg_mark = period_div.select('.module-avg-mark')[0].text  # Средний балл
+
+        subject = {'name': name, 'marks': marks, 'avg_mark': avg_mark}
+
+        subjects.append(subject)
+
+    return subjects
+
+
 def get_week_info(diary_page):
     page = BS(diary_page, 'html.parser')
     try:
         return page.select('h3[_ngcontent-c18]')[0].text
     except:
         return False
+
+
+def get_grade_and_school(diary_page):
+    page = BS(diary_page, 'html.parser')
+    grade = page.select('.header[_ngcontent-c15] .ezd-header h2')[0].text
+    grade = re.findall(r',.+', grade)[0][2:]
+
+    school = page.select('.dropdown-profile-current')[0].text
+    school = re.findall(r'\w.+', school)[0]
+
+    return grade, school
 
 
 def get_fio(diary_page):
@@ -116,4 +193,6 @@ if __name__ == '__main__':
     #
     # print(text)
 
-    get_week_info(get_page())
+    # get_week_info(get_page())
+
+    print(get_marks(get_page2(), 1))
